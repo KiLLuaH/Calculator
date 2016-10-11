219898e654e05852bdb0419813dda0fead2bc093
Volkan <Volkan>
2016-10-11 18:42:30 +0200

    added user input

--- Calculator.java
+++ Calculator.java
@@ -2,5 +2,9 @@ import java.util.*;
 public class Calculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Bitte eingeben [+,-,*,/] Zahl1 Zahl2: ");
+		String operator = scanner.next();
+		int zahl1 = scanner.nextInt();
+		int zahl2 = scanner.nextInt();
 	}
 }
